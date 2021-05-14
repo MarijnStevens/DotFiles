b2ecfd2ba32dce2edc73b5db9f6c818a7a76b497
Marijn Stevens <marijnstevens@gmail.com>
2021-05-14 11:21:17 +0200

    Fixed typo in cross-compile-rust-build.sh

--- scripts/rust/cross-compile-rust-build.sh
+++ scripts/rust/cross-compile-rust-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#TARGET="aarch-unknown-linux-gnueabi" R3/4 64build
+#TARGET="aarch64-unknown-linux-gnu" R3/4 64build
 #TARGET="armv7-unknown-linux-gnueabif" # R2/R3
 TARGET="arm-unknown-linux-gnueabif" # R 0/1
 
