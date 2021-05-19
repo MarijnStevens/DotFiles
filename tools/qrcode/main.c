5eb073caa1be6ea91336cc355ae6603524958419
Marijn Stevens <marijnstevens@gmail.com>
2021-05-19 21:09:26 +0200

    Replaced qrcode generation code.

--- tools/qrcode/main.c
+++ tools/qrcode/main.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "qrcodegen.h"
 
-#include "qrcode.h"
+// Prints the given QR Code to the console.
+static void printQr(const uint8_t qrcode[]) {
+	int size = qrcodegen_getSize(qrcode);
+	int border = 0;
+	for (int y = -border; y < size + border; y++) {
+		for (int x = -border; x < size + border; x++) {
+			fputs((qrcodegen_getModule(qrcode, x, y) ? "\u2588\u2588" : "  "), stdout);
+		}
+		fputs("\n", stdout);
+	}
+	fputs("\n", stdout);
+}
 
 int main(int argc, char **argv)
 {
-	int qrVersion = 6;
-	int qrErr = 1;
-	int maxSize = 512*1024;
-	char buffer[maxSize];
-	read(STDIN_FILENO, buffer, maxSize);
+   	int maxSize = 512*1024;
+   	char buffer[maxSize];
 
-    // Create the QR code
-	QRCode qrcode;
+    // User-supplied text
+   	read(STDIN_FILENO, buffer, maxSize);
+	  enum qrcodegen_Ecc errCorLvl = qrcodegen_Ecc_MEDIUM;  // Error correction level
 
-	// Allocate a chunk of memory to store the QR code
-    uint8_t qrcodeData[qrcode_getBufferSize(qrVersion)];
-
-	if(qrcode_initText(&qrcode, qrcodeData, qrVersion, qrErr, buffer) < 0)
-	{
-		printf("qrcode: To much data to generate qrcode from. \n");
-		return -1;
-	}
-
-    for (uint8_t y = 0; y < qrcode.size; y++) {
-        for (uint8_t x = 0; x < qrcode.size; x++) {
-            // Print each module (UTF-8 \u2588 is a solid block)
-            printf(qrcode_getModule(&qrcode, x, y) ? "\u2588\u2588": "  ");
-        }
-
-        printf("\n");
+	  // Make and print the QR Code symbol
+	  uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
+	  uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+	  bool ok = qrcodegen_encodeText(buffer, tempBuffer, qrcode, errCorLvl,
+		  qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+	  if (ok)
+		{
+      printQr(qrcode);
+    }
+    else
+    {
+      printf("Failed to generate QR code.");
     }
 }
